77ba05f3842ea141fa6ae06a5876d3070e30fb01
tokarev_pa <tokarevpavel1996@mail.ru>
2025-09-27 12:07:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Auth
+
+## OpenAPI
+Локальная разработка: http://localhost:8080/swagger-ui/index.html
---
+++ src/main/java/ru/copperside/auth/config/properties/AppInfoProperties.java
@@ -0,0 +1,13 @@
+package ru.copperside.auth.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app-info")
+public class AppInfoProperties {
+    private String version;
+    private String artifactId;
+}
